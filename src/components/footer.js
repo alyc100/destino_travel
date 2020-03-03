8fc61e42185845548d472d4898e1f3a761dd7416
Alex Lee <alyc100@hotmail.com>
2020-03-03 22:56:54 +0000

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,10 +16,33 @@ export default () => (
 							</div>
 						</div>
 						<div class="footer_about_text">Destino Travel Agency IS NOT A REAL COMPANY.</div>
-						
+
 					</div>
 				</div>
+        <div class="col-lg-4 footer_col">
+					<div class="footer_latest">
+						<div class="footer_title">Latest News</div>
+						<div class="footer_latest_content">
 
+							<div class="footer_latest_item">
+								<div class="footer_latest_image"><img src="/images/latest_1.jpg"></div>
+								<div class="footer_latest_item_content">
+									<div class="footer_latest_item_title"><a href="#">Brazil Summer</a></div>
+									<div class="footer_latest_item_date">Jan 09, 2020</div>
+								</div>
+							</div>
+
+							<div class="footer_latest_item">
+								<div class="footer_latest_image"><img src="/images/latest_2.jpg"></div>
+								<div class="footer_latest_item_content">
+									<div class="footer_latest_item_title"><a href="#">A perfect vacation</a></div>
+									<div class="footer_latest_item_date">Jan 09, 2020</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</footer>
